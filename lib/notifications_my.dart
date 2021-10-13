960232cf82a71fb6ed20a4e9fb848d9feafcf0dc
dumptyhumpty2014 <dumptyhumpty2014@gmail.com>
2021-10-13 19:24:52 +0300

    новый плагин уведомлений

--- lib/notifications_my.dart
+++ lib/notifications_my.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 const myChannelId = 'dumptyhumptychannelId';
@@ -23,18 +24,38 @@ class NotificationsMy {
         onSelectNotification: selectNotification);
   }
 
-  Future<void> pushNotification(String title, String text) async {
-    const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-      myChannelId, //id channel по нему удалять
-      myChannelName, //channel name
-      'channel from exmaple', //description
-      importance: Importance.max,
-      priority: Priority.high,
-      showWhen: false,
-      enableVibration: true,
-    );
-    const NotificationDetails platformChannelSpecifics =
+  Future<void> pushNotification(String title, String text,
+      {String? soundName}) async {
+    AndroidNotificationDetails androidPlatformChannelSpecifics;
+    if (soundName == null) {
+      androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        myChannelId, //id channel по нему удалять
+        myChannelName, //channel name
+        channelDescription: 'channel from exmaple', //description
+        importance: Importance.max,
+        priority: Priority.high,
+        showWhen: true,
+        enableVibration: true,
+        groupKey: 'com.android.background_notification',
+        setAsGroupSummary: true,
+        color: Colors.green,
+      );
+    } else {
+      androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        '${myChannelId}_${soundName}', //id channel по нему удалять
+        '${myChannelName}_${soundName}', //channel name
+        channelDescription: 'channel from exmaple _${soundName}', //description
+        importance: Importance.max,
+        priority: Priority.high,
+        showWhen: true,
+        enableVibration: true,
+        sound: RawResourceAndroidNotificationSound('sound${soundName}'),
+        //TODO icon:
+        groupKey: 'com.android.background_notification',
+        setAsGroupSummary: true,
+      );
+    }
+    NotificationDetails platformChannelSpecifics =
         NotificationDetails(android: androidPlatformChannelSpecifics);
     await flutterLocalNotificationsPlugin
         .show(0, title, text, platformChannelSpecifics, payload: 'item x');
